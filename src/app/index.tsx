1146759583bee2f5905c0805b8e977ced77e611c
Yash Rathod <yashrathod0226@gmail.com>
2024-10-20 01:28:46 +0530

    Added StatusBar Background color

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,14 @@
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StatusBar, StyleSheet, Text, View } from "react-native";
 import products from "@assets/data/products";
 import ProductListItem from "@components/ProductListItem";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Index() {
+export default function MenuScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar
+        backgroundColor={'red'}
+      />
       <FlatList
         data={products}
         renderItem={({ item }) => <ProductListItem product={item} />}
