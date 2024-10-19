90a82ef054b116d7f12dfb2500a1bf1191b023a7
Yash Rathod <yashrathod0226@gmail.com>
2024-10-20 00:07:11 +0530

    Building UI

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import products from "@/assets/data/products";
+import { Colors } from "../constants/Colors";
 
 export default function Index() {
+  const product = products[0];
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World!!!</Text>
+    <View style={styles.container} >
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 10
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold'
+  }
+})
